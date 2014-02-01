6c57bd0d807ca668746bbbf1c9b3e808f48f88ff
Deyan Ginev <d.ginev@jacobs-university.de>
2014-02-01 18:26:12 +0100

    moving to Research 1, commiting on the way ....

--- erato.pl
+++ erato.pl
@@ -1,9 +1,17 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use Erato::Analyze qw(anaylze_artist);
 
 get '/' => sub {
   my $self = shift;
   $self->render('index');
 };
+
+post '/analyze' => sub {
+  my $self = shift;
+  my $post_params = $self->req->body_params->params || [];
+  my $report = map {anaylze_artist($_)} @$post_params;
+  $self->render(json => $report);
+}
 
 app->start;
